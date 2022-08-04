61d0697f970e387b9a5a99e873ee5251e6dfe357
berluseden <91573071+berluseden@users.noreply.github.com>
2022-08-04 09:20:56 -0400

    Update seed.groovy

--- dsl/seed.groovy
+++ dsl/seed.groovy
@@ -1,19 +1,3 @@
-job('DockerBuildandPublishDSL') {
-     scm {
-        git {
-        remote {
-                url('https://github.com/berluseden/nodeapp_test.git')
-            }
-            wrappers {
-                preBuildCleanup()
-            } 
-            branch('*/' + 'main')
-        }
-    }
-
-    steps {
-        dockerBuildAndPublish {
-            repositoryName('berluseden/demojenkins')
-            registryCredentials('dockerhub')
-        }
-    }
+job(jobName) {
+    description("A simple Freestyle Job created from seed.groovy")
+}
